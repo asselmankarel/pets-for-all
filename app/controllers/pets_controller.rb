4745a4b7e7e5edc8a75fd8ab7fb913cfbc7995e4
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-09 19:59:35 +0100

    Implemented edit, update and destroy/ Added corresponding links and view pages(edit form)

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,6 +23,22 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path
+  end
+
   private
 
   def pet_params
