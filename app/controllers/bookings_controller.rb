1e48f1c3179dd986e7bd8697607226a78a2f22c4
Karel Asselman <asselman.karel@gmail.com>
2020-11-10 11:00:25 +0100

    implemented bookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,53 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  # new_pet_booking    GET    /pets/:pet_id/bookings/new(.:format)        bookings#new
+  #                    POST   /pets/:pet_id/bookings(.:format)            bookings#create
+  # edit_pet_booking   GET    /pets/:pet_id/bookings/:id/edit(.:format)   bookings#edit
+  # pet_booking        GET    /pets/:pet_id/bookings/:id(.:format)        bookings#show
+  #                    PATCH  /pets/:pet_id/bookings/:id(.:format)        bookings#update
+  #                    DELETE /pets/:pet_id/bookings/:id(.:format)
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def new
+    @booking = Booking.new
+    @pet = Pet.find(params[:pet_id])
+    @booking.user = current_user
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.safe
+      redirect_to pet_booking @booking
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to pet_booking @booking
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy if @booking.user == current_user
+    redirect_to pet_bookings
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :pet_id)
+  end
 end
