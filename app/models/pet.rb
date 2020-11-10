37b68c4d7d0c9b37513e63a5e75d637b88598c75
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-10 11:23:04 +0100

    Feedback implemented, Calendar implemented

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,10 +2,13 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  CATEGORIES = %i[Dog Cat Horse Reptile Rodent Dragon Unicorn]
+  GENDERS = %i[Male Female Unkwoned]
+
   validates :name, presence: true
   validates :address, presence: true
-  validates :gender, presence: true, inclusion: { in: ["male", "female"] }
+  validates :gender, presence: true, inclusion: { in: GENDERS }
   validates :birth_date, presence: true
   validates :price_per_day, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :category, presence: true, inclusion: { in: %i[dog cat horse reptile rodent dragon unicorn] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
