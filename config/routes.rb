3bcf79e4fbdb7e0029eec53ae306b20b53b954f3
Karel Asselman <asselman.karel@gmail.com>
2020-11-10 12:15:05 +0100

    bookingsController#edit and destroy + views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pets do
-    resources :bookings
+    resources :bookings, only: [:index, :new, :show, :create, :edit, :update]
   end
   # to allow a user to see all his bookings instead of going via the pet
+  resources :bookings, only: [:destroy]
   get '/users/:id/bookings', to: 'bookings#index', as: 'user_bookings'
 end
