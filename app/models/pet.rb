092154c8d285c6a4f92e8a8bfb17d88b4269fabc
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-10 11:43:51 +0100

    Feedback implemented, plus price_per_day now allows floats

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,13 +2,13 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  CATEGORIES = %i[Dog Cat Horse Reptile Rodent Dragon Unicorn]
-  GENDERS = %i[Male Female Unkwoned]
+  CATEGORIES = %w[Dog Cat Horse Reptile Rodent Dragon Unicorn]
+  GENDERS = %w[Male Female Unkwoned]
 
   validates :name, presence: true
   validates :address, presence: true
   validates :gender, presence: true, inclusion: { in: GENDERS }
   validates :birth_date, presence: true
-  validates :price_per_day, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price_per_day, presence: true, numericality: { only_float: true, greater_than: 0 }
   validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
