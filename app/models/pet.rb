25eb54b5b1537faae02e593f52ba13732146ed7b
Pedro Vilarinho <Pedro.Vilarinho.a@outlook.com>
2020-11-10 10:57:34 +0100

    Implemented validations for Pets in petmodel

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,11 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :gender, presence: true, inclusion: { in: ["male", "female"] }
+  validates :birth_date, presence: true
+  validates :price_per_day, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :category, presence: true, inclusion: { in: %i[dog cat horse reptile rodent dragon unicorn] }
 end
