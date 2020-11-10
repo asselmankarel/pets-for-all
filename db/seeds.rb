5486ae12d19b81ff10c487cee172314f9191be96
Karel Asselman <asselman.karel@gmail.com>
2020-11-10 14:36:25 +0100

    rounded price seed to 2 decimals

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ user.save!
 
 # create 10 pets
 30.times do
-  pet = Pet.new(name: Faker::Name.first_name , birth_date: Faker::Date.birthday(min_age: 1, max_age: 10), category: Pet::CATEGORIES.sample , gender: Pet::GENDERS.sample ,available: true, price_per_day: rand(1.1..49.9),address: Faker::Address.street_address)
+  pet = Pet.new(name: Faker::Name.first_name , birth_date: Faker::Date.birthday(min_age: 1, max_age: 10), category: Pet::CATEGORIES.sample , gender: Pet::GENDERS.sample ,available: true, price_per_day: rand(1.1..49.9).round(2),address: Faker::Address.street_address)
   pet.user = user
   pet.save!
 end
