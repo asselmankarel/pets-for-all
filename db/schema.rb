ef2223aec3d86cfb0a29782ea241f4afdd83db4e
Karel Asselman <asselman.karel@gmail.com>
2020-11-11 21:37:57 +0100

    added confirmed column to bookings table, updated booking partial view and added view link under avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_093938) do
+ActiveRecord::Schema.define(version: 2020_11_11_200404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_093938) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "confirmed", default: false
     t.index ["pet_id"], name: "index_bookings_on_pet_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
