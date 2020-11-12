c24342ec014180742e1608e6f7f5a25cf2bc209c
Karel Asselman <asselman.karel@gmail.com>
2020-11-12 16:28:22 +0100

    dashboard added bookings for owned pets

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,16 @@ class DashboardsController < ApplicationController
 
   def index
     @pets = Pet.where(user: current_user)
+    find_bookings
+  end
+
+  private
+
+  def find_bookings
+    all_bookings = Booking.where("end_date >= '#{DateTime.now}'")
+    @bookings = []
+    all_bookings.each do |booking|
+      @bookings << booking if booking.pet.user == current_user
+    end
   end
 end
